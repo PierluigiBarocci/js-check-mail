36c4513122e8e5cbc3ad51937d6e075bbe9fd9da
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-15 15:00:51 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,4 @@
+// Esercitazione 1 => repo: js-check-mail
+// Creare un array contenente una lista di email.
+// Attraverso un prompt chiedere all'utente la sua email.
+// Controllare se l'email dell'utente è presente nell'array e stampare un messaggio appropriato.
